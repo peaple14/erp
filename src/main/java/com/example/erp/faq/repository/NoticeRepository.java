fade49ada99935b20be4eb9e60199f801e325853
tkdwns02kj <tkdwns02kj@cu.ac.kr>
2023-10-30 17:23:30 +0900

    공지사항,견적서포함

--- src/main/java/com/example/erp/faq/repository/NoticeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.erp.faq.repository;
-
-import com.example.erp.faq.entity.NoticeEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NoticeRepository  extends JpaRepository<NoticeEntity,Long> {
-}
